567adb17709750041672b006632089e8051d1f1f
Sean Lang <slang800@gmail.com>
2019-02-23 12:47:53 -0600

    make it work as a python package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='three_oh_one',
+      version='0.1',
+      description='',
+      author='Sean Lang',
+      author_email='slang800@gmail.com',
+      license='GPL-3.0',
+      packages=['three_oh_one'],
+      zip_safe=False)
